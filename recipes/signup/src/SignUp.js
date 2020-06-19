1e226302632672e9e5c596447713b51fe6ecc2b1
JenVest2020 <jenvest2019@gmail.com>
2020-06-18 22:51:23 -0500

    working on radio button issues

--- recipes/signup/src/SignUp.js
+++ recipes/signup/src/SignUp.js
@@ -7,8 +7,26 @@ import * as yup from 'yup';
 
 const SignUp = () => {
     const [collapsed, setCollapsed] = useState(true);
-
+    const [formData, setFormData] = useState({
+        name: '',
+        username: '',
+        password: '',
+        private: '',
+        public: ''
+    });
     const toggleNavbar = () => setCollapsed(!collapsed);
+    const onInputChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+    const handlepreferences = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.checked
+        });
+    };
 
     return (
         <div style={{ background: '#ebcd4' }}>
@@ -34,45 +52,55 @@ const SignUp = () => {
                     </Nav>
                 </Collapse>
             </Navbar>
-            <img style={{ width: '100%' }} src={MarketingPage} alt='' />
+            <img style={{ width: '100%', height: '40vh' }} src={MarketingPage} alt='' />
             <Form style={{ width: '80%', margin: '0 auto', textAlign: 'center' }}>
                 <FormGroup>
                     <legend>Name</legend>
                     <input style={{ border: '2px ridge #9e5110' }}
                         type='name'
-                        name='name' />
+                        name='name'
+                        value={formData.name}
+                        onChange={onInputChange} />
                 </FormGroup>
                 <FormGroup>
                     <legend>Choose a User Name</legend>
                     <input style={{ border: '2px ridge #9e5110' }}
                         type='text'
-                        name='username' />
+                        name='username'
+                        value={formData.username}
+                        onChange={onInputChange} />
                 </FormGroup>
                 <FormGroup>
                     <legend>Choose a Password</legend>
                     <input style={{ border: '2px ridge #9e5110' }}
                         type='password'
-                        name='password' />
+                        name='password'
+                        value={formData.password}
+                        onChange={onInputChange} />
                 </FormGroup>
                 <div style={{ width: '30%', margin: '0 auto', border: '2px ridge #9e5110', background: '#b17537', color: 'white' }}>
                     <h5>Do you want your recipes to be searchable for others?</h5>
-                    <FormGroup check>
-                        <label check>
-                            <input
-                                type='checkbox'
-                                name='private'
-                                checked={false} />
+                    <FormGroup tag='fieldset'>
+                        <FormGroup check>
+                            <label check>
+                                <input
+                                    type='radio'
+                                    name='private'
+                                    value='private'
+                                    onChange={onInputChange} />
                         Private(not others)
                     </label>
-                    </FormGroup>
-                    <FormGroup check>
-                        <label check>
-                            <input
-                                type='checkbox'
-                                name='public'
-                                checked={false} />
+                        </FormGroup>
+                        <FormGroup check>
+                            <label check>
+                                <input
+                                    type='radio'
+                                    name='public'
+                                    value='public'
+                                    onChange={onInputChange} />
                             Public(all others)
                     </label>
+                        </FormGroup>
                     </FormGroup>
                 </div>
                 <Button style={{ color: 'white', background: '#b17537', border: '2px ridge #9e5110', margin: '2%' }}>Submit</Button>
