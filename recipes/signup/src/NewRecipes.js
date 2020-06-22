b87b7b1832529d9a596aa9ad42b4fa1048110c53
JenVest2020 <jenvest2019@gmail.com>
2020-06-22 17:38:10 -0500

    working on recipe form functionality

--- recipes/signup/src/NewRecipes.js
+++ recipes/signup/src/NewRecipes.js
@@ -1,15 +1,43 @@
 import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 import VintageRecipeCardwithTab from '../src/VintageRecipeCardwithTab.png';
+import Axios from 'axios';
 
 const NewRecipes = () => {
     const [collapsed, setCollapsed] = useState(true);
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [formData, setFormData] = useState({
+        category: '',
+        title: '',
+        description: '',
+        ingredients: '',
+        directions: '',
+        private: true
+    });
+
+    const onInputChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handlePrefrence = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.checked
+        });
+    };
 
     const toggle = () => setDropdownOpen(prevState => !prevState);
 
     const toggleNavbar = () => setCollapsed(!collapsed);
+
+    const submitData = (e) => {
+        e.preventDefault();
+        // Axios.post('')
+    }
     return (
         <div>
             <Navbar style={{ background: '#b17537' }} light>
@@ -26,47 +54,97 @@ const NewRecipes = () => {
             </Navbar>
             <Dropdown isOpen={dropdownOpen} toggle={toggle}>
                 <DropdownToggle style={{ background: '#ebcdb4', color: 'black', fontWeight: 'bold', border: '2px solid #43260f' }} caret>
-                    Category
-          </DropdownToggle>
+                    {formData.category === '' ? 'Category' : formData.category}
+                </DropdownToggle>
                 <DropdownMenu style={{ color: 'black' }}>
-                    <DropdownItem style={{ fontWeight: 'bold' }}>Appetizers</DropdownItem>
-                    <DropdownItem style={{ fontWeight: 'bold' }}>Main Course</DropdownItem>
-                    <DropdownItem style={{ fontWeight: 'bold' }}>Side Dish</DropdownItem>
-                    <DropdownItem style={{ fontWeight: 'bold' }}>Deserts & Breads</DropdownItem>
-                    <DropdownItem style={{ fontWeight: 'bold' }}>Drinks</DropdownItem>
+                    <div onClick={() => {
+                        toggle();
+                        setFormData({ ...formData, category: 'Appetizers' })
+                    }}>
+                        Appetizers
+                            </div>
+                    <div onClick={() => {
+                        toggle();
+                        setFormData({ ...formData, category: 'Main Course' })
+                    }}>
+                        Main Course
+                            </div>
+
+                    <div onClick={() => {
+                        toggle();
+                        setFormData({ ...formData, category: 'Side Dish' })
+                    }}>
+                        Side Dish
+                            </div>
+                    <div onClick={() => {
+                        toggle();
+                        setFormData({ ...formData, category: 'Deserts&Breads' })
+                    }}>
+                        Deserts&Breads
+                            </div>
+                    <div onClick={() => {
+                        toggle();
+                        setFormData({ ...formData, category: 'Drinks' })
+                    }}>
+                        Drinks
+                            </div>
                     <DropdownItem divider />
-                    <DropdownItem style={{ fontWeight: 'bold' }}>Christmas</DropdownItem>
-                    <DropdownItem style={{ fontWeight: 'bold' }}>ThanksGiving</DropdownItem>
-                    <DropdownItem style={{ fontWeight: 'bold' }}>Easter</DropdownItem>
+                    <div onClick={() => {
+                        toggle();
+                        setFormData({ ...formData, category: 'Christmas' })
+                    }}>
+                        Christmas
+                            </div>
+                    <div onClick={() => {
+                        toggle();
+                        setFormData({ ...formData, category: 'Thanksgiving' })
+                    }}>
+                        Thanksgiving
+                            </div>
+                    <div onClick={() => {
+                        toggle();
+                        setFormData({ ...formData, category: 'Easter' })
+                    }}>
+                        Easter
+                            </div>
                 </DropdownMenu>
             </Dropdown>
-            <Form style={{ width: '80%', margin: '0 auto', textAlign: 'center' }} >
+            <Form style={{ width: '80%', margin: '0 auto', textAlign: 'center' }}
+                onSubmit={submitData}>
                 <FormGroup>
                     <legend style={{ color: 'black', fontWeight: 'bold', marginBottom: '0' }}>Title</legend>
                     <Input
                         type='text'
                         name='title'
-                        style={{ border: '2x ridge #b17537' }} />
+                        value={formData.title}
+                        style={{ border: '2x ridge #b17537' }}
+                        onChange={onInputChange} />
                 </FormGroup>
                 <FormGroup>
                     <legend style={{ color: 'black', fontWeight: 'bold', marginBottom: '0' }}>Description</legend>
                     <Input style={{ border: '2x solid #43260f' }}
                         type='textarea'
-                        name='desctiption' />
+                        name='description'
+                        value={formData.description}
+                        onChange={onInputChange} />
                 </FormGroup>
                 <FormGroup>
                     <legend style={{ color: 'black', fontWeight: 'bold', marginBottom: '0' }}>Ingredients</legend>
                     <Input style={{ border: '2x solid #b17537' }}
                         type='textarea'
-                        name='title' />
+                        name='ingredients'
+                        value={formData.Ingredients}
+                        onChange={onInputChange} />
                 </FormGroup>
                 <FormGroup>
                     <legend style={{ color: 'black', fontWeight: 'bold', marginBottom: '0' }}>Directions</legend>
                     <Input style={{ border: '2x solid #b17537' }}
                         type='textarea'
-                        name='title' />
+                        name='directions'
+                        value={formData.directions}
+                        onChange={onInputChange} />
                 </FormGroup>
-                <FormGroup tag='fieldset'>
+                {/* <FormGroup tag='fieldset'>
                     <legend style={{ color: 'black', fontWeight: 'bold' }} >
                         Special appliances needed?
               </legend>
@@ -110,8 +188,8 @@ const NewRecipes = () => {
                             checked={false} />{' '}
             Blender
           </Label>
-                </FormGroup>
-                <div style={{ background: '#ebcdb4', color: 'black', fontWeight: 'bold' }}>
+                </FormGroup> */}
+                <div style={{ background: '#ebcdb4', border: '2px ridge #b17537', color: 'black', fontWeight: 'bold' }}>
                     <FormGroup tag='fieldset'>
                         <legend>Uncheck the box below if you want this recipe to be searchable for the public</legend>
                         <FormGroup check>
@@ -119,12 +197,16 @@ const NewRecipes = () => {
                                 <Input
                                     type='checkbox'
                                     name='private'
-                                    checked={true} />
+                                    checked={formData.checked}
+                                    onChange={handlePrefrence} />
                         Private
                     </label>
                         </FormGroup>
                     </FormGroup>
                 </div>
+                <Button style={{ background: '#ebcdb4', color: 'black', fontWeight: 'bold', border: '2px solid #43260f' }}>
+                    Submit
+                </Button>
             </Form>
         </div >
     )
